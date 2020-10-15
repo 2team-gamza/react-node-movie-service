8c710239bd63f7a5288766c4e9ca2fda18e40b44
seungh0 <will.seungho@gmail.com>
2020-10-16 02:33:39 +0900

    백엔드 코드 추가

---
+++ back/src/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+app.use(express.json());
+app.use(cors()); // CORS 설정
+
+const knex = require("knex");
+const db = knex({
+  client: "mysql",
+  connection: {
+    host: "localhost",
+    user: "root",
+    database: "movie_db",
+    password: "0212", // .gitignore 처리 해야함.
+  },
+}); // DB 연결 부분
+
+app.get("/movies", (req, res) => {
+  db.raw(`SELECT * FROM movie`)
+    .then((response) => {
+      res.status(200).send(response[0]);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).end("Error");
+    });
+});
+
+app.post("/movie", (req, res) => {
+  const name = req.body.name;
+  const price = req.body.price;
+
+  db.raw(`INSERT INTO movie(name, price) VALUES("${name}", ${price})`)
+    .then((response) => {
+      res.status(200).send("SUCCESS");
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).end("Error");
+    });
+});
+
+app.listen(5000, () => {
+  console.log("Server is running now!");
+});
